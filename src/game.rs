a59ab60b17fb9223e33c1874d3e8ad8b16e8fd77
Julien PAUSE <julien.pause@epitech.eu>
2020-10-16 17:23:24 +0200

    Create struct game with field board, and add unit tests for it

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,32 @@
-struct Game;
+const NB_SQUARES: usize = 40;
+
+struct Game {
+    board: [i8; NB_SQUARES],
+}
+
+impl Game {
+    fn create() -> Game {
+        let game_board: [i8; NB_SQUARES] = [0; NB_SQUARES];
+        Game { board: game_board }
+    }
+}
 
 #[cfg(test)]
-mod game_test {
+mod tests {
     use super::*;
 
     #[test]
     fn can_create_game() {
-        let _game: Game;
+        let _: Game;
+        let _: Game = Game::create();
+    }
+
+    #[test]
+    fn board_is_initialized() {
+        let game = Game::create();
+
+        for i in 0..NB_SQUARES {
+            assert_eq!(game.board[i], 0);
+        }
     }
 }
