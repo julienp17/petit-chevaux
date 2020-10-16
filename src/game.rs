d4992d1655db3a37c53be0d5584795b5d710fa0a
Julien PAUSE <julien.pause@epitech.eu>
2020-10-16 17:40:33 +0200

    Move print_board function from main to method of Game struct

--- src/game.rs
+++ src/game.rs
@@ -1,14 +1,41 @@
+extern crate colored;
+
+use colored::*;
 const NB_SQUARES: usize = 40;
 
-struct Game {
+pub struct Game {
     board: [i8; NB_SQUARES],
 }
 
 impl Game {
-    fn create() -> Game {
+    pub fn create() -> Game {
         let game_board: [i8; NB_SQUARES] = [0; NB_SQUARES];
         Game { board: game_board }
     }
+
+    pub fn print_board(self) {
+        let colors = [
+            Colorize::on_red("[H]"),
+            Colorize::on_yellow("[H]"),
+            Colorize::on_green("[H]"),
+            Colorize::on_blue("[H]"),
+        ];
+        let mut color_index = 0;
+
+        for square in 0..NB_SQUARES {
+            if is_print_starting_square(square) {
+                print!("{}", colors[color_index]);
+                color_index += 1;
+            } else {
+                print!("[ ]");
+            }
+        }
+        println!();
+    }
+}
+
+fn is_print_starting_square(square_number: usize) -> bool {
+    return square_number % 10 == 0;
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
-extern crate colored;
 mod game;
-
-use colored::*;
-
-const HORSE_SYMBOL: &str = "H";
-const NB_SQUARES: i8 = 40;
+use crate::game::Game;
 
 fn main() {
-    print_board();
-}
-
-fn print_board() {
-    let colors = [
-        Colorize::on_red("[H]"),
-        Colorize::on_yellow("[H]"),
-        Colorize::on_green("[H]"),
-        Colorize::on_blue("[H]"),
-    ];
-    let mut color_index = 0;
-
-    for square in 0..NB_SQUARES {
-        if is_starting_square(square) {
-            print!("{}", colors[color_index]);
-            color_index += 1;
-        } else {
-            print!("[ ]");
-        }
-    }
-    println!();
-}
-
-fn is_starting_square(square_number: i8) -> bool {
-    return (square_number - 1) % 10 == 0;
+    let my_game = Game::create();
+    my_game.print_board();
 }
